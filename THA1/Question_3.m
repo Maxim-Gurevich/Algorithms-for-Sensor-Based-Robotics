d44dc25477bff5f96cecaae3dc6d6566310985ca
Zahin Nambiar <32119274+Zahin-Nambiar@users.noreply.github.com>
2021-02-27 14:34:49 -0600

    Added Q3 file and config_calc function

---
+++ THA1/Question_3.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = Question_3(inputArg1,inputArg2)
+%QUESTION_3 Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
---
+++ THA1/configuration_calculator.m
@@ -0,0 +1,9 @@
+function [config_1,config_2,config_3,config_4] = configuration_calculator(screw,theta)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+config_1 = screw;
+config_2 = screw;
+config_3 = theta;
+config_4 = theta;
+end
+
