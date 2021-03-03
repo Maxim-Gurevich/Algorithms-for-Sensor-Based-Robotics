1fc40d3058b1833e606c61f73eee4d80254c72a6
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-03-02 20:06:37 -0600

    nvm yellow not done yet

--- THA1/Question_3.m
+++ THA1/Question_3.m
@@ -1,4 +1,4 @@
-function [outputArg1,outputArg2] = Question_3(q,s_hat,h,theta,T)
+function [outputArg1,outputArg2] = Question_3(q,s_hat,h,theta,Tinit)
 
 % type this into command line to run: 
 % Question_3([0 2 0],[0 0 1], 2, pi,[1 0 0 2;0 1 0 0; 0 0 1 0;0 0 0 1])
@@ -6,7 +6,8 @@ function [outputArg1,outputArg2] = Question_3(q,s_hat,h,theta,T)
 % QUESTION_3 Summary of this function goes here
 %   Detailed explanation goes here
 
-screw=[s_hat;cross(-s_hat,q)+h.*s_hat];
+twist=[s_hat;cross(-s_hat,q)+h.*s_hat];
+
 [config_1,config_2,config_3,config_4] = ...
     configuration_calculator(screw,theta,Tinit);
 [S, theta2] = TMatrix2ScrewAngle(config_4);
