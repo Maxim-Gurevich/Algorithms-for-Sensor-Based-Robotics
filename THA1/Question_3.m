30c03a99ed538bc5528cf2bf0715869dcf4d83ad
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-03-02 20:16:39 -0600

    works all the way up to tmatrix2screwangle

--- THA1/Question_3.m
+++ THA1/Question_3.m
@@ -5,9 +5,14 @@ function [outputArg1,outputArg2] = Question_3(q,s_hat,h,theta,Tinit)
 
 % QUESTION_3 Summary of this function goes here
 %   Detailed explanation goes here
+omega=s_hat;
+v=cross(-s_hat,q)+h.*s_hat;
+%twist=[omega;v];
 
-twist=[s_hat;cross(-s_hat,q)+h.*s_hat];
-
+screw=[0 -omega(3) omega(2) v(1);
+    omega(3) 0 -omega(1) v(2);
+    -omega(2) omega(1) 0 v(3);
+    0 0 0 0];
 [config_1,config_2,config_3,config_4] = ...
     configuration_calculator(screw,theta,Tinit);
 [S, theta2] = TMatrix2ScrewAngle(config_4);
