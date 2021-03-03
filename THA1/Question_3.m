d5fab67c332113f83b0bb9a2c9232339dbc9e433
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-03-02 18:36:23 -0600

    its a start

--- THA1/Question_3.m
+++ THA1/Question_3.m
@@ -1,7 +1,14 @@
-function [outputArg1,outputArg2] = Question_3(inputArg1,inputArg2)
+function [outputArg1,outputArg2] = Question_3(screw,theta,Tinit)
 %QUESTION_3 Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+
+[config_1,config_2,config_3,config_4] = ...
+    configuration_cal(screw,theta,Tinit);
+[S, theta2] = TMatrix2ScrewAngle(config_4);
+
+tp = theaterPlot();
+op = orientationPlotter(tp,'DisplayName','Fused Data',...
+    'LocalAxesLength',2);
+    plotOrientation(op, [config_1;config_2;config_3;config_4])
 end
 
