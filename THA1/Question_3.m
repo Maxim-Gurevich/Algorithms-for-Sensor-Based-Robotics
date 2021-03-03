d2ab87af9fda07b64d3cbf5a6892c593d9da4ae2
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-03-02 20:42:15 -0600

    update

--- THA1/Question_3.m
+++ THA1/Question_3.m
@@ -5,20 +5,24 @@ function [outputArg1,outputArg2] = Question_3(q,s_hat,h,theta,Tinit)
 
 % QUESTION_3 Summary of this function goes here
 %   Detailed explanation goes here
+
+%W4-L1: The Screw Interpertation of a Twist
 omega=s_hat;
 v=cross(-s_hat,q)+h.*s_hat;
-%twist=[omega;v];
 
 screw=[0 -omega(3) omega(2) v(1);
     omega(3) 0 -omega(1) v(2);
     -omega(2) omega(1) 0 v(3);
     0 0 0 0];
+
 [config_1,config_2,config_3,config_4] = ...
     configuration_calculator(screw,theta,Tinit);
+
 [S, theta2] = TMatrix2ScrewAngle(config_4);
 
+%plot
 tp = theaterPlot();
 op = orientationPlotter(tp,'DisplayName','Data',...
     'LocalAxesLength',2);
-    plotOrientation(op, [config_1;config_2;config_3;config_4])
+    plotOrientation(op, [body_1;body_2;body_3;body_4])
 end
