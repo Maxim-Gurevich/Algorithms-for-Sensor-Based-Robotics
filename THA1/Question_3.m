aea882009b988caf1563a06f353157431791de85
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-03-02 19:27:50 -0600

    added command line example in comments

--- THA1/Question_3.m
+++ THA1/Question_3.m
@@ -1,10 +1,15 @@
 function [outputArg1,outputArg2] = Question_3(q,s_hat,h,theta,T)
-%QUESTION_3 Summary of this function goes here
+
+% type this into command line to run: 
+% Question_3([0 2 0],[0;0;1], 2, pi,[1 0 0 2;0 1 0 0; 0 0 1 0;0 0 0 1])
+
+% QUESTION_3 Summary of this function goes here
 %   Detailed explanation goes here
 
-screw=[0 -screwin(3) screwin(2) ;
-       screwin(3) 0 -screwin(1);
-       -screwin(2) screwin(1) 0];
+-s_hat*q+h.*s_hat
+
+screw=[s_hat;
+    -s_hat*q+h.*s_hat];
 [config_1,config_2,config_3,config_4] = ...
     configuration_calculator(screw,theta,Tinit);
 [S, theta2] = TMatrix2ScrewAngle(config_4);
