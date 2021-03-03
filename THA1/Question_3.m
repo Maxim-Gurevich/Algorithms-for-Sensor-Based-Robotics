672d17147596cd864686b436458f0a816ad84dc4
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-03-02 19:04:43 -0600

    update

--- THA1/Question_3.m
+++ THA1/Question_3.m
@@ -1,7 +1,10 @@
-function [outputArg1,outputArg2] = Question_3(Tinit,screw,theta)
+function [outputArg1,outputArg2] = Question_3(q,s_hat,h,theta,T)
 %QUESTION_3 Summary of this function goes here
 %   Detailed explanation goes here
 
+screw=[0 -screwin(3) screwin(2) ;
+       screwin(3) 0 -screwin(1);
+       -screwin(2) screwin(1) 0];
 [config_1,config_2,config_3,config_4] = ...
     configuration_calculator(screw,theta,Tinit);
 [S, theta2] = TMatrix2ScrewAngle(config_4);
@@ -11,4 +14,3 @@ op = orientationPlotter(tp,'DisplayName','Data',...
     'LocalAxesLength',2);
     plotOrientation(op, [config_1;config_2;config_3;config_4])
 end
-
