297b12e78987eb42155c64bb93387e046587e578
Zahin Nambiar <32119274+Zahin-Nambiar@users.noreply.github.com>
2021-03-04 00:09:42 -0600

    finished my portion of debugs for problem 3

--- THA1/main.m
+++ THA1/main.m
@@ -1,6 +1,6 @@
 %Test cases%
 %%
-%1. angle_axis_func
+%1a. angle_axis_func
 I1 = [0 1 2 3;4 5 6 7;8 9 10 11;12 13 14 15];
 I2 = [0 -1 0;1 0 0;0 0 1];
 I3 = [0 0 1;1 0 0;0 1 0];
@@ -14,7 +14,7 @@ I5 = [3 2 2;3 4 5];
 [m,n,o] = angle_axis_func(I5)
 
 %%
-%2. quaternion_func.m
+%1b. quaternion_func.m
 I1 = [0 1 2 3;4 5 6 7;8 9 10 11;12 13 14 15];
 I2 = [0 -1 0;1 0 0;0 0 1];
 I3 = [0 0 1;1 0 0;0 1 0];
@@ -28,7 +28,7 @@ I5 = [3 2 2;3 4 5];
 [m1,n1] = quaternion_func(I5)
 
 %%
-%3. euler_angle_func.m
+%1c. euler_angle_func.m
 I1 = [0 1 2 3;4 5 6 7;8 9 10 11;12 13 14 15];
 I2 = [0 -1 0;1 0 0;0 0 1];
 I3 = [0 0 1;1 0 0;0 1 0];
@@ -42,3 +42,21 @@ I6 = eye(3);
 [m2,n2,o2] = euler_angle_func(I5)
 [p2,q2,r2] = euler_angle_func(I6)
 %%
+%3a. configuration_calculator.m
+Screw1 = [0 0 3 0;0 0 0 1;-3 0 0 0;0 0 0 0];
+Screw2 = [0 0 0 0;0 0 0 1;0 0 0 0;0 0 0 0];
+Theta1 = 0;
+Theta2 = 1;
+Tinit = [0,-2,0,1;2,0,0,0;0,0,0,0;0,0,0,1];
+
+[a3,b3,c3,d3] = configuration_calculator(Screw1,Theta1,Tinit)
+[e3,f3,g3,h3] = configuration_calculator(Screw1,Theta2,Tinit)
+[i3,j3,k3,l3] = configuration_calculator(Screw2,Theta2,Tinit)
+[m3,n3,o3,p3] = configuration_calculator(Screw2,Theta1,Tinit)
+%%
+%3b. screw2qsh.m
+S1 = [0 -1 .333 0;1 0 -.333 0;-.333 .333 0 , 1;0 0 0 0];
+S2 = [0 -.333 .333 0;.333 0 -.333 0;-.333 .333 0 , 1;0 0 0 0];
+
+[a4,b4,c4] = screw2qsh(S1)
+[d4,e4,f4] = screw2qsh(S2)
