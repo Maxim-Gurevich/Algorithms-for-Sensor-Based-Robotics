da66389bb61e93e4bad8fe7352e2bc3b20a2f06d
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-03-04 18:11:58 -0600

    update

--- THA1/Bonus_Programming_4.m
+++ THA1/Bonus_Programming_4.m
@@ -1,4 +1,11 @@
 function [within,R]=Bonus_Programming_4(A,threshold)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Description: Returns boolean and similar SO(3) matrix to input
+% Input: 3x3 matrix
+% Output: boolean, simmilar matrix
+% Date: 3/3/21 
+% Author: Maxim Gurevich
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     [M,diff]=Bonus_Math_10(A);
     if diff>threshold
         within=false;
