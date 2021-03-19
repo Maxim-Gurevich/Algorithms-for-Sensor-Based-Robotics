3ef18626089acf5a79f318988b21b994a7edac30
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-03-19 14:23:36 -0500

    update

--- THA2/FK_space.m
+++ THA2/FK_space.m
@@ -15,11 +15,32 @@
     % xyz="0.0 0.13585 0.0"/>
     % <axis xyz="0 1 0"/>
     
+%sample end effector info (can be extracted from URDF)
+roll=0;
+pitch=0;
+yaw=0;
+x=1;
+y=1;
+z=1;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
 %determines M from data pertaining to last link in URDF
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 M=zeros(4); %initilaize M
-M(1:4,1:3)=[1 0 0; 0 1 0; 0 0 1; 0 0 0];
+
+%convert roll pitch yaw to rotation matrix (should ...
+%be made into its own funciton)
+a=yaw;
+b=pitch;
+g=roll;
+R=[cos(a)*cos(b) cos(a)*sin(b)*sin(g)-sin(a)*cos(g) cos(a)*sin(b)*cos(g)+sin(a)*sin(g);
+    sin(a)*cos(b) sin(a)*sin(b)*sin(g)+cos(a)*cos(g) sin(a)*sin(b)*cos(g)-cos(a)*sin(g);
+    -sin(b) cos(b)*sin(g) cos(b)*cos(g)];
+
+%set appropriate part of M
+M(1:3,1:3)=R;
+
+%set xyz part
+M(1:4,4)=[x;y;z;1];
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
