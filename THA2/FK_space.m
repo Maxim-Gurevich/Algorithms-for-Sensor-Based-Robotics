d889a34c2f4794fe748082a30d22e1370e1530e7
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-03-19 11:19:16 -0500

    Create FK_space.m
    began THA2 PA

---
+++ THA2/FK_space.m
@@ -0,0 +1,7 @@
+% given URDF, generate space form FK
+
+%calculates M(1:3,1:3) by assuming a given orientation of the end effector
+
+%calculates M(1:3,4) by finding the sum of all links in 3D
+
+%for each joint, determine the screw axis
