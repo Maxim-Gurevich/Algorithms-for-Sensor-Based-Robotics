64e5fcc9dc3e8d1393afd973ff48dd79da7f8f06
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-03-29 13:50:26 -0500

    update

--- THA2/J_space.m
+++ THA2/J_space.m
@@ -2,15 +2,15 @@
 clc
 clear all
 theta=[0 0 0 0 0 0];
-T=FK_space(theta);
+[T,S,S_matrix,M]=FK_space(theta);
 
 
-J_s=zeros(size(theta),6);
-J_s(1)=S(1);
+J_s=zeros(length(theta),6);
+J_s(:,1)=S(:,1);
 term=1;
-for i=2:size(theta,6)
+for i=2:length(theta)
     for j=1:i-1
-        term=term*exp(S(i)*theta(i));
+        term=term*exp(S_matrix(:,:,i)*theta(i));
     end
-    J_s(i)=adjoint(term)*S(i);
+    J_s(i)=adjoint(term)*S(:,i);
 end
