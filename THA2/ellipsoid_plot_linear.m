e3da690455f2319d9d3bc0fda11ef3836deeb33f
Zahin Nambiar <32119274+Zahin-Nambiar@users.noreply.github.com>
2021-03-31 00:28:33 -0500

    Update ellipsoid_plot_linear.m

--- THA2/ellipsoid_plot_linear.m
+++ THA2/ellipsoid_plot_linear.m
@@ -1,7 +1,21 @@
-function [outputArg1,outputArg2] = ellipsoid_plot_linear(inputArg1,inputArg2)
+function [eig_vals,eig_vects] = ellipsoid_plot_linear(jacobian)
 %ELLIPSOID_PLOT_LINEAR Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+dims = size(jacobian);
+columns = dims(2);
+Jv = jacobian(4:6,1:columns);
+A = Jv*transpose(Jv);
+[eigenvalues,eigenvector] = eig(A); 
+eig_vals = diag(eigenvalues);%eigenvalues in a 1D vector
 
+[X,Y,Z] = ellipsoid(0,0,0,eig_vals(1),eig_vals(2),eig_vals(3));
+surf(X,Y,Z);
+title('Linear Ellipsoid Plot');
+grid on
+xlabel('v1')
+ylabel('v2')
+zlabel('v3')
+axis equal
+eig_vects = eigenvector; %eigenvectors are the columns
+
+end
