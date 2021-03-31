ffef4e0bfccf21a615346554fcf385a0eb8af0a8
Zahin Nambiar <32119274+Zahin-Nambiar@users.noreply.github.com>
2021-03-30 23:22:23 -0500

    Update singularity.m

--- THA2/singularity.m
+++ THA2/singularity.m
@@ -1,8 +1,14 @@
 function state = singularity(jacobian)
 %SINGULARITY Summary of this function goes here
-%Input = 6x6 jacobian 
+%Input = 6xn jacobian 
 %returns 1 if in singularity, returns 0 if not in singularity
-if det(jacobian) == 0
+dims = size(jacobian);
+if dims(2)>6
+    max_rank = 6;
+else
+    max_rank = dims(2);
+end
+if rank(jacobian) < max_rank
     state = 1;
 else
     state = 0;
