0b015627ab24d9b298529d394ab9dd1f70467afd
Zahin Nambiar <32119274+Zahin-Nambiar@users.noreply.github.com>
2021-04-01 16:19:21 -0500

    added main file for debugging test cases

---
+++ THA2/main.m
@@ -0,0 +1,25 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Description: Debugging all the subfunctions for PA 2
+% Input: 
+% Output: 
+% Date: 3/30/21 
+% Author: Zahin Nambiar,Maxim Gurevich
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%1.FK_space.m
+%2.FK_body.m
+%3.J_space.m
+%4.J_body.m
+%5.singularity.m
+%6.ellipsoid_plot_angular.m
+%7.ellipsoid_plot_linear.m
+%8.J_isotropy.m
+%9.J_condition.m
+%10.J_ellipsoid_volume.m
+%11.J_inverse_kinematics.m
+%12.J_transpose_kinematics.m
+%13.redundancy_resolution.m
+%14.DLS_inverse_kinematics.m
+%15.BONUS_SIM.m
+%16.config.m
+%17.Roboplot.m
