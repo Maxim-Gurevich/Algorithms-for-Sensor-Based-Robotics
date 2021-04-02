89474c132e37f0e83e740b493c92793dd3522a5e
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-04-01 22:37:20 -0500

    update

--- THA2/J_transpose_kinematics.m
+++ THA2/J_transpose_kinematics.m
@@ -9,33 +9,31 @@ function theta=J_transpose_kinematics(T_sd,theta)
 % Date: 3/30/21 
 % Author: Maxim
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%theta=[0;0;0;0;0;0;.1];
-FK_body(theta,1);%to initiate graph
-[J_b]=J_body(theta);
-%T_sd=M(:,:,end);
-constant=.5;
+[T,B1,B_matrix,M]=FK_body(theta,1);%to initiate graph
+%[J_b]=J_body(theta,0);
+%T_sd=M(:,:,end)
+constant=.6;
 i=0;
 omega_b=[10 10 10];
 v_b=[10 10 10];
-eps_omega=.01;
-eps_v=.01;
+eps_omega=.1;
+eps_v=.1;
+theta=transpose(theta);
 while (norm(omega_b)>eps_omega || norm(v_b) > eps_v) && i<100
-    V_b_matrix=logm(inv(FK_body(theta,2))*T_sd);
+    [T,~,~,~]=FK_body(theta,0);
+    J_b=J_body(theta,2);
+    T_bd=inv(T)*T_sd;
+    V_b_matrix=logm(T_bd);
     v_b=[V_b_matrix(3,2); V_b_matrix(1,3); V_b_matrix(2,1)];
     omega_b=V_b_matrix(1:3,4);
     V_b=[v_b; omega_b];
     theta=theta+constant*transpose(J_b)*V_b;
-    i=i+1;
-    v_b=[V_b_matrix(3,2); V_b_matrix(1,3); V_b_matrix(2,1)];
-    omega_b=V_b_matrix(1:3,4);
+    i=i+1
 end
 if i==100
     error('theta could not converge')
 end
 end
-
-
 
 
 
