85a2ad6e23baa581c4d5c964132bac5d3b1b9e1d
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-04-10 10:53:06 -0500

    update

--- THA2/DEMO.m
+++ THA2/DEMO.m
@@ -2,3 +2,7 @@ clc
 clear all
 close all
 
+theta=J_inverse_kinematics([1 0 0 .1;
+                            0 1 0 .5;
+                            0 0 1 .3;
+                            0 0 0 1],[0 0 0 0 0 0])
