d35773f811f25ffc87531aa9df1dc658f314f04d
Zahin Nambiar <32119274+Zahin-Nambiar@users.noreply.github.com>
2021-04-24 22:45:42 -0500

    4 complete

---
+++ HW3-ASBR/HW3-PA1/PA1P4.m
@@ -0,0 +1,1 @@
+[b_tip,b_post] = pivot_calibration('HW3-PA1_DATA/pa1-debug-a-empivot.txt')
--- HW3-ASBR/HW3-PA1/pivot_calibration.m
+++ HW3-ASBR/HW3-PA1/pivot_calibration.m
@@ -7,7 +7,7 @@ function [b_tip,b_post] = pivot_calibration(pivot_calib_data)
 % Date: 4/23/21 
 % Author: Zahin Nambiar
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-A = importdata('HW3-PA1_DATA/'+pivot_calib_data);
+A = importdata(pivot_calib_data);
 probe_markers = str2double(cell2mat(A.textdata(1)));
 data_frames = str2double(cell2mat(A.textdata(2)));
 data = A.data;
@@ -26,18 +26,19 @@ for i=1:data_frames
    end
 end
 
-R_matrix = zeros(data_frames*probe_markers,2);
+R_matrix = zeros(data_frames*probe_markers*3,6);
 p_matrix = zeros(data_frames*probe_markers,1);
 for i=1:data_frames*probe_markers
-   R_matrix(i,2) = -1*eye(3);
+   R_matrix(3*(i-1)+1:3*(i-1)+3,4:6) = -1*eye(3);
    T = set_registration(data1,data);
-   R_matrix(i,1) = T(1:3,1:3);
-   p_matrix(i,1) = -1*T(1:3,4);
+   R_matrix(3*(i-1)+1:3*(i-1)+3,1:3) = T(1:3,1:3);
+   p_matrix(3*(i-1)+1:3*(i-1)+3,1) = -1*T(1:3,4);
 end
-b = inv(R_matrix)*p_matrix;
 
-b_tip = b(1);
-b_post = b(2);
+b = pinv(R_matrix)*p_matrix;
+
+b_tip = b(1:3,1);
+b_post = b(4:6,1);
 end
 
 
