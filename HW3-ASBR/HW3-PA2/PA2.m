4f8ad919fdf6ad90635c7a38cf7a46c7e465c670
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-04-26 18:55:39 -0500

    update (works, but output looks off

--- HW3-ASBR/HW3-PA2/PA2.m
+++ HW3-ASBR/HW3-PA2/PA2.m
@@ -27,42 +27,47 @@ R_x=Q*sqD*inv(Q)*M';
 
 %solve for t_x using least squares
 for i=1:10
-    E(i,:)=t_A-R_x*t_B;
-    F(i,:)=eye(3)-R_A(i);
+    t_A=t_Robot_config(i,:)';
+    t_B=t_camera_config(i,:)';
+    R_A=Quat2RotMat(q_Robot_config(i,:));
+    E(3*(i-1)+1:3*(i-1)+3,:)=t_A-R_x*t_B;
+    F(3*(i-1)+1:3*(i-1)+3,:)=eye(3)-R_A;
 end
-%F*t_x=E;
+t_x=pinv(F)*E;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %quaternion method
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Solve for R_x
 for i=1:10
-    s_a=q_a(1);
-    v_a=q_a(2:4);
-    s_b=q_b(1);
-    v_b=q_b(2:4);
+    s_a=q_Robot_config(i,1);
+    v_a=q_Robot_config(i,2:4);
+    s_b=q_camera_config(i,1);
+    v_b=q_camera_config(i,2:4);
     vec=v_a+v_b;
     skew=[0 -vec(3) vec(2);
         vec(3) 0 -vec(1);
         -vec(2) vec(1) 0];
-    M(:,:,i)=[s_a-s_b (v_a-v_b)';
-        v_a-v_b (s_a-s_b)*eye(3)+skew];
+    M(3*(i-1)+1,1)=s_a-s_b;
+    M(3*(i-1)+1,2:4)=(v_a-v_b)';
+    M(3*(i-1)+2:3*(i-1)+4,1)=v_a-v_b;
+    M(3*(i-1)+2:3*(i-1)+4,2:4)=(s_a-s_b)*eye(3)+skew;
 end
 [U,S,V]=svd(M);
 y=[0;0;0;1];
 q_x=V'*y;
 
 %solve for translation vector p_x
-%least squares -- A'*A*x=A'*b
-R_x=quat2rotmat(q_x);
-%R_ak=info from function
-%R_bk=info from function
-%p_ak=info from funciton
-%p_bk=info from function
-ATA=(R_ak-eye(3))'*(R_ak-eye(3));
-ATb=(R_ak-eye(3))*(R_x*p_bk-p_ak);
-p_x=ATb/ATA;
-    
+R_xq=Quat2RotMat(q_x);
+for i=1:10
+    R_A=Quat2RotMat(q_Robot_config(i,:));
+    t_a=t_Robot_config(i,:)';
+    t_b=t_camera_config(i,:)';
+    E(3*(i-1)+1:3*(i-1)+3,:)=R_xq*t_a-t_a;
+    F(3*(i-1)+1:3*(i-1)+3,:)=R_A-eye(3);
+end
+t_xq=pinv(F)*E;
+
     
     
     
