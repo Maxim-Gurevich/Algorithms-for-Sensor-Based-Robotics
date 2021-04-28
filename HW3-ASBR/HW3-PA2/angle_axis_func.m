5eee2107e3674618333c85b1ce9e079b4d3b7df4
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-04-28 17:15:30 -0500

    update

---
+++ HW3-ASBR/HW3-PA2/angle_axis_func.m
@@ -0,0 +1,25 @@
+function [angle,axis,valid] = angle_axis_func(R)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Description: Calculating angle and axis from rotation matrix
+% Input: Square Matrix (nxn)
+% Output: Validity of input matrix as rotation matrix and angle axis
+% representation if so
+% Date: 3/1/21 
+% Author: Zahin Nambiar
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    dim = size(R);
+    if (length(dim)~=2 | dim(1)~=dim(2) | dim(1)~=3 | det(R)~=1)
+         valid = "Not valid R";
+         angle = 0;
+         axis = 0;
+    else
+        angle = acos(.5*(trace(R)-1));
+        axis = (R-transpose(R))/(2*sin(angle));
+        if (angle == 0)
+            valid = "Not valid R";
+        else
+            valid = "Valid R";
+        end
+    end
+end
+
