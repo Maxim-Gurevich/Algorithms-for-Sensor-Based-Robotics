1d1198fbc1f6bbbe985dc4fa35ce9faef418c0f1
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-05-10 09:23:10 -0500

    update

--- HW4-ASBR/b.m
+++ HW4-ASBR/b.m
@@ -5,19 +5,19 @@ close all
 p_goal = [0.3;0.15;0.12]; %Goal for p_tip to move to (in space frame)
 radius = .003; %Acceptable radius from p_goal
 p_tip = [0.0;0.0;0.1]; %Ptip coordinates (last frame of the robot chain)
-q = [.5;.5;.5;.5;.5;.5]; %initial theta values
+q = [0;0;0;0;0;0;0]; %initial theta values
 [TT,~,~,~]=FK_space(q,1);
 
-t = TT(:,:,6)*[p_tip;1];
+t = TT(:,:,7)*[p_tip;1];
 t = t(1:3,:);
 
 i=0;
-while (norm(t-p_goal) > radius)&&(i<10)
+while (norm(t-p_goal) > radius)&&(i<100)
 J_s = J_space(q,0);
 R=TT(1:3,1:3,6);
 t_skew = -1.*[0 -t(3) t(2); t(3) 0 -t(1); -t(2) t(1) 0];
 Rz_skew=[0 -R(3,3) R(2,3); -R(3,3) 0 -R(1,3); -R(2,3) R(1,3) 0];
-C = (t_skew*J_s(1:3,:) + J_s(4:6,:))+(-Rz_skew*J_s(1:3,:));
+C = (t_skew*J_s(1:3,:) + J_s(4:6,:));%+(-Rz_skew*J_s(1:3,:));
 d = p_goal - t;
 
 delta_q = lsqlin(C,d,[],[],[],[],-q,2*pi()-q);
