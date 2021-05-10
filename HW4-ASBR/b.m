53157158796a32f04e1ac8208729e96aa222447d
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-05-10 13:52:55 -0500

    Update b.m

--- HW4-ASBR/b.m
+++ HW4-ASBR/b.m
@@ -16,17 +16,17 @@ t = t(1:3,:);
 i=0;
 while (norm(t-p_goal) > radius)&&(i<100)
 J_s = J_space(q,0);
-R=TT(1:3,1:3,7);
+R=TT(1:3,1:3,1);
 t_skew = -1.*[0 -t(3) t(2); t(3) 0 -t(1); -t(2) t(1) 0];
 Rz_skew=[0 -R(3,3) R(2,3); -R(3,3) 0 -R(1,3); -R(2,3) R(1,3) 0];
 C = t_skew*J_s(1:3,:) + J_s(4:6,:)+10*(-Rz_skew*J_s(1:3,:));
 d = p_goal - t;
-A=t_skew*J_s(1:3,:) + J_s(4:6,:)
+A=t_skew*J_s(1:3,:) + J_s(4:6,:);
 w=100*ones(3,1);
 
 delta_q = lsqlin(C,d,[],[],[],[],...
     [],[]);%-q,2*pi()-q)
-q = q + delta_q
+q = q + delta_q;
 [TT,~,~,~]=FK_space(q,2);
 
 t = TT(:,:,7)*[p_tip;1];
