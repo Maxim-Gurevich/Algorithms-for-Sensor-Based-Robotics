c42ce697fd76429c2fe450779ef369395c7a8422
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-05-10 11:43:02 -0500

    update

--- HW4-ASBR/test.m
+++ HW4-ASBR/test.m
@@ -4,8 +4,8 @@ close all
 
 T=[1 0 0 .1;
     0 1 0 .5;
-    0 0 1 .1;
+    0 0 1 0;
     0 0 0  1];
-starting_angles=[0 0 0 0 0 0 0];
+starting_angles=[-2.5 0 -.5 1.5 -1.25 1 1];
 
-theta_red=redundancy_resolution(T,starting_angles);
+theta_red=redundancy_resolution(T,starting_angles)
