3bb3e5c21a1c736440a546c3a6ef66718d6393c6
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-05-10 16:15:48 -0500

    update

--- HW4-ASBR/a.m
+++ HW4-ASBR/a.m
@@ -3,7 +3,7 @@ clear all
 close all
 
 p_goal = [0.3;0.15;0.12]; %Goal for p_tip to move to (in space frame)
-p_goal = [0.1;.5;.1];
+%p_goal = [0.1;.5;.1];
 radius = .003; %Acceptable radius from p_goal
 p_tip = [0.0;0.0;0.1]; %Ptip coordinates (last frame of the robot chain)
 q = [.5;.5;.5;.5;.5;.5]; %initial theta values
@@ -18,7 +18,7 @@ t_skew = -1.*[0 -t(3) t(2); t(3) 0 -t(1); -t(2) t(1) 0];
 C = t_skew*J_s(1:3,:) + J_s(4:6,:);
 d = p_goal - t;
 
-delta_q = lsqlin(C,d,[],[],[],[],-pi-q,pi-q;
+delta_q = lsqlin(C,d,[],[],[],[],-pi-q,pi-q);
 q_o = q;
 q = q + delta_q;
 [TT,~,~,~]=FK_space(q,2);
