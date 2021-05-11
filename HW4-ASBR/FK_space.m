2d594d300b64bfa93e0f4af3a02655d2b6e84c75
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-05-10 19:33:05 -0500

    update

--- HW4-ASBR/FK_space.m
+++ HW4-ASBR/FK_space.m
@@ -3,32 +3,32 @@ function [T_s,S,S_matrix,M]=FK_space(theta,graph)
 % Description: % generate space form FK using...
 % information typically found in URDF
 % Input: theta, graph mode (0-no graph, 1-new graph,
-                    %2-add to old graph
+%2-add to old graph
 % Output: plot, transformation, screw axes, M
-% Date: 3/30/21 
+% Date: 3/30/21
 % Author: Maxim
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [rpy,xyz,axis_xyz]=config();
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %determines M for all joints
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 M=zeros(4,4,length(theta)); %initilaize M
 for i=1:length(theta)
-%convert roll pitch yaw to rotation matrix
-a=rpy(i,3);
-b=rpy(i,2);
-g=rpy(i,1);
-R=[cos(a)*cos(b) cos(a)*sin(b)*sin(g)-sin(a)*cos(g)...
-    cos(a)*sin(b)*cos(g)+sin(a)*sin(g);
-    sin(a)*cos(b) sin(a)*sin(b)*sin(g)+cos(a)*cos(g)...
-    sin(a)*sin(b)*cos(g)-cos(a)*sin(g);
-    -sin(b) cos(b)*sin(g) cos(b)*cos(g)];
-
-%set rotation part of M
-M(1:3,1:3,i)=R;
-
-%set xyz part
-M(1:4,4,i)=[xyz(i,1);xyz(i,2);xyz(i,3);1];
+    %convert roll pitch yaw to rotation matrix
+    a=rpy(i,3);
+    b=rpy(i,2);
+    g=rpy(i,1);
+    R=[cos(a)*cos(b) cos(a)*sin(b)*sin(g)-sin(a)*cos(g)...
+        cos(a)*sin(b)*cos(g)+sin(a)*sin(g);
+        sin(a)*cos(b) sin(a)*sin(b)*sin(g)+cos(a)*cos(g)...
+        sin(a)*sin(b)*cos(g)-cos(a)*sin(g);
+        -sin(b) cos(b)*sin(g) cos(b)*cos(g)];
+    
+    %set rotation part of M
+    M(1:3,1:3,i)=R;
+    
+    %set xyz part
+    M(1:4,4,i)=[xyz(i,1);xyz(i,2);xyz(i,3);1];
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -37,28 +37,30 @@ end
 S=zeros(6,length(theta));
 S_matrix=zeros(4,4,length(theta));
 T_s=zeros(4,4,length(theta));
-orientations=zeros(3,3,length(theta));
-positions=zeros(length(theta),3);
+orientations=zeros(3,3,length(theta)+1);
+positions=zeros(length(theta)+1,3);
 for i=1:length(theta)
     for j = 1:i
-       v=cross(-axis_xyz(j,:),xyz(j,:));
-       %for each joint, determine the screw axis
-       S(:,j)=[axis_xyz(j,:) v];
-       S_matrix(:,:,j)=[0  -axis_xyz(j,3) axis_xyz(j,2)  v(1);
-             axis_xyz(j,3)       0        -axis_xyz(j,1) v(2);
+        v=cross(-axis_xyz(j,:),xyz(j,:));
+        %for each joint, determine the screw axis
+        S(:,j)=[axis_xyz(j,:) v];
+        S_matrix(:,:,j)=[0  -axis_xyz(j,3) axis_xyz(j,2)  v(1);
+            axis_xyz(j,3)       0        -axis_xyz(j,1) v(2);
             -axis_xyz(j,2) axis_xyz(j,1)       0         v(3);
-                        0        0             0          0];
-       if j==1
-           T_si=expm(S_matrix(:,:,j)*theta(j));
-       else
-           T_si=T_si*expm(S_matrix(:,:,j)*theta(j));
-       end
+            0        0             0          0];
+        if j==1
+            T_si=expm(S_matrix(:,:,j)*theta(j));
+        else
+            T_si=T_si*expm(S_matrix(:,:,j)*theta(j));
+        end
     end
     T_s(:,:,i)=T_si*M(:,:,i);
     orientations(:,:,i)=inv(T_s(1:3,1:3,i));
     positions(i,:)=[T_s(1,4,i) T_s(2,4,i) T_s(3,4,i)];
 end
 
+orientations(:,:,i+1)=inv(T_s(1:3,1:3,i));
+positions(i+1,:)=[T_s(1,4,i) T_s(2,4,i) T_s(3,4,i)];
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %plot
--- HW4-ASBR/b.m
+++ HW4-ASBR/b.m
@@ -3,9 +3,10 @@ clear all
 close all
 
 p_goal = [0;0.5;0]; %Goal for p_tip to move to (in space frame)
-radius = .01; %Acceptable radius from p_goal
+radius = .001; %Acceptable radius from p_goal
 p_tip = [0.0;0.0;0.1]; %Ptip coordinates (last frame of the robot chain)
-q = [-2.5;0;-.5;1.5;-1.25;1;1];
+q = redundancy_resolution([eye(3) p_goal;0 0 0 1],zeros(1,7));
+%[-2.5;0;-.5;1.5;-1.25;1;1];
 qmin=-5;
 qmax=5;
 [TT,~,~,~]=FK_space(q,1);
@@ -19,18 +20,18 @@ J_s = J_space(q,0);
 R=TT(1:3,1:3,7);
 t_skew = -1.*[0 -t(3) t(2); t(3) 0 -t(1); -t(2) t(1) 0];
 Rz_skew=[0 -R(3,3) R(2,3); -R(3,3) 0 -R(1,3); -R(2,3) R(1,3) 0];
-C = t_skew*J_s(1:3,:) + J_s(4:6,:);%+100*(-Rz_skew*J_s(4:6,:));
-d = p_goal - t;
+C = t_skew*J_s(1:3,:) + J_s(4:6,:)+0*(-Rz_skew*J_s(4:6,:));
+d = p_goal-t;
 % A=[cos(u1i) cos(u1i) cos(u1i) sin(u1i) sin(u1i) 0 0 0;
 %     ];
 w=100*ones(3,1);
-delta_q = lsqlin(C,d,[],[],[],[],-.001,.001);
+delta_q = lsqlin(C,d,[],[],[],[],[],[]);
 q = q + delta_q;
 [TT,~,~,~]=FK_space(q,2);
 t = TT(:,:,7)*[p_tip;1];
 t = t(1:3,:);
 %Roboplot('points',t)
-norm(t-p_goal);
+norm(t-p_goal)
 i=i+1
 end
 
