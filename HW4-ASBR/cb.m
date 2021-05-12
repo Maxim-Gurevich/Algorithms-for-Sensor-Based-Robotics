d31b01f65209169cb2b969bf1e92a477170af319
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-05-12 18:03:18 -0500

    update

--- HW4-ASBR/cb.m
+++ HW4-ASBR/cb.m
@@ -2,18 +2,17 @@ clc
 clear all
 close all
 
-p_goal = [0;0.4;0.1];%Goal for p_tip to move to (in space frame)
-p_start = [.2;0.4;0.3];
-plane_point=[0 0 10];
+p_goal = [0.2;0.4;0.3];%Goal for p_tip to move to (in space frame)
+p_tip = [0;0;0.1]; %Ptip coordinates (last frame of the robot chain)
+p_start = [0.2;0.4;0.299]-p_tip;
+plane_point=[0 0 0];
 plane_normal=[0 0 1];
 radius = .001; %Acceptable radius from p_goal
-p_tip = [0.0;0;0.1]; %Ptip coordinates (last frame of the robot chain)
 q = redundancy_resolution([eye(3) p_start;0 0 0 1],...
     [2 2.5 -1.7 -1.5 0.3 -1.5 -2.4]);
-qmin=-5;
-qmax=5;
 [TT,~,~,~]=FK_space(q,1);
-
+limits=[-pi -pi/4 -pi -pi -pi -pi -2*pi;
+    pi 2*pi pi/3 0 pi pi 0];
 %plot plane
 patch([.1 -.1 -.1 .1], [.1 .1 -.1 -.1],...
     [1 1 1 1],[0 0 0 0])
@@ -22,7 +21,8 @@ patch([.1 -.1 -.1 .1], [.1 .1 -.1 -.1],...
 t = TT(:,:,7)*[p_tip;1];
 t = t(1:3,:);
 plane_dist=plane_normal*(t-plane_point');
-i=0;
+i=1;
+tplot(:,i)=t;
 while (norm(t-p_goal) > radius)&&(i<100)
     J_s = J_space(q,0);
     J_b = J_body(q,0);
@@ -32,13 +32,14 @@ while (norm(t-p_goal) > radius)&&(i<100)
     t_skew = [0 -t(3) t(2); t(3) 0 -t(1); -t(2) t(1) 0];
     Rz_skew=[0 -Rz(3) Rz(2); -Rz(3) 0 -Rz(1); -Rz(2) Rz(1) 0];
     C = [1*(-t_skew*J_b(1:3,:) + J_b(4:6,:));
-        1*(-Rz_skew*J_b(1:3,:))];
+        1*(-Rz_skew*J_b(1:3,:)) 0 0 0];
     d = [p_goal-t;0;0;0];
-    A = [[-plane_normal 0 0 0]*J_b];
-    w=A*[(plane_normal.*plane_dist)';0;0;0;0];
-    delta_q = lsqlin(C,d,A,w,[],[],[],[]);
-    %[-pi -pi/4 -pi -pi -pi -pi -2*pi]'-q,...
-    %[pi pi pi/3 0 pi pi 0]'-q);
+    A = plane_normal*J_b(4:6,:);
+    w=[plane_normal]*[plane_point-t']';
+    qmin=limits(1,:)'-q;
+    qmax=limits(2,:)'-q;
+    delta_q = lsqlin(C,d,[],[],A,w,[],[]);
+    A*delta_q-w
     q = q + delta_q;
     pause(.1);
     [TT,~,~,~]=FK_space(q,2);
@@ -48,5 +49,7 @@ while (norm(t-p_goal) > radius)&&(i<100)
     norm(t-p_goal)
     plane_dist=plane_normal*(t-plane_point');
     i=i+1
+    tplot(:,i)=t;hold on
+    plot3(tplot(1,:),tplot(2,:),tplot(3,:),'r')
+    hold off
 end
-
