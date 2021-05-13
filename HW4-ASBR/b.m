e3eb7253b34f1a9b6639996d06e67d86b282f408
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-05-13 14:02:18 -0500

    Update b.m

--- HW4-ASBR/b.m
+++ HW4-ASBR/b.m
@@ -7,7 +7,7 @@ function []=b(p_start,p_goal)
 % Author: Maxim+Zahin
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %p_goal = [0;0.5;0]; %Goal for p_tip to move to (in space frame)
-radius = .0001; %Acceptable radius from p_goal
+radius = .001; %Acceptable radius from p_goal
 p_tip = [0.0;0.0;0.1]; %Ptip coordinates (last frame of the robot chain)
 p_start = p_start-p_tip;
 q = redundancy_resolution([eye(3) p_start;0 0 0 1],...
@@ -22,8 +22,8 @@ t = t(1:3,:);
 i=0;
 while (norm(t-p_goal) > radius)&&(i<100)
 J_b = J_body(q,0);
-R=TT(1:3,1:3,1);
-Rz=R*[0;0;1];
+R=TT(1:3,1:3,7)
+Rz=R*[0;0;1]
 t_skew = [0 -t(3) t(2); t(3) 0 -t(1); -t(2) t(1) 0];
 Rz_skew=[0 -Rz(3) Rz(2); -Rz(3) 0 -Rz(1); -Rz(2) Rz(1) 0];
 C = [1*(-t_skew*J_b(1:3,:) + J_b(4:6,:));
@@ -36,16 +36,15 @@ w=[.03;.03;.03];
 delta_q = lsqlin(C,d,A,w,[],[],[],[]);
      %[-pi -pi/4 -pi -pi -pi -pi -2*pi]'-q,...
      %[pi pi pi/3 0 pi pi 0]'-q);
-options.Display = 'iter';
 q = q + delta_q;
 pause(.1);
 [TT,~,~,~]=FK_space(q,2);
 [Tb,~,~,~]=FK_body(q,0);
 t = TT(:,:,7)*[p_tip;1];
-R = t
 t = t(1:3,:);
-norm(t-p_goal)
-i=i+1
+error=norm(t-p_goal)
+i=i+1;
+iterations=i
 end
 end
 
