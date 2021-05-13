585401ea85baf54fd1908affa52d074069cb5f79
Maxim-Gurevich <65980337+Maxim-Gurevich@users.noreply.github.com>
2021-05-13 13:30:43 -0500

    Update a.m

--- HW4-ASBR/a.m
+++ HW4-ASBR/a.m
@@ -1,3 +1,4 @@
+function []=a(p_start,p_goal)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Description: uses lsqlin to move probot to point
 % Input:
@@ -6,10 +7,10 @@
 % Author: Maxim+Zahin
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-p_goal = [0;0.5;0]; %Goal for p_tip to move to (in space frame)
+%p_goal = [0;0.5;0]; %Goal for p_tip to move to (in space frame)
 radius = .0001; %Acceptable radius from p_goal
 p_tip = [0.0;0.0;0.1]; %Ptip coordinates (last frame of the robot chain)
-p_start = [0.2;0.4;0.299]-p_tip;
+p_start = p_start-p_tip;
 q = redundancy_resolution([eye(3) p_start;0 0 0 1],...
     [2 2.5 -1.7 -1.5 0.3 -1.5 -2.4]);
 %q = [0;0;0;0;0;0;0]; %initial theta values
@@ -27,13 +28,14 @@ C = t_skew*J_b(1:3,:) + J_b(4:6,:);
 d = p_goal - t;
 delta_q = lsqlin(C,d,[],[],[],[],[],[]);%...
 %    (-2*pi-q),(2*pi-q));
-q = q + delta_q;
+q = q + delta_q
 [TT,~,~,~]=FK_space(q,2);
 %J_inverse_kinematics(TT(:,:,6),q_o);
 
 t = TT(:,:,7)*[p_tip;1];
 t = t(1:3,:);
-norm(t-p_goal)
-i = i+1
+error=norm(t-p_goal)
+i=i+1;
+iterations=i
 end
-
+end
